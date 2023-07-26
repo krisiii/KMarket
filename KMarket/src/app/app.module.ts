54a41bd51be4eef923942f8859dc323e62b816aa
Krisi Stoyanova <kstoyanova06@gmail.com>
2023-07-26 16:42:38 +0300

    Login, Register, Dropdown
    Only HTML code and still not the CSS

--- KMarket/src/app/app.module.ts
+++ KMarket/src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     CoreModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    UserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ KMarket/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase : {
+        apiKey: "AIzaSyDUDwRWF0oRFkTOEAZrj1WJQFohB95iUjc",
+        authDomain: "kmarket-7a9c1.firebaseapp.com",
+        projectId: "kmarket-7a9c1",
+        storageBucket: "kmarket-7a9c1.appspot.com",
+        messagingSenderId: "129933621955",
+        appId: "1:129933621955:web:2b9b5b00a81f6b8f874076"
+      },
+    //   apiUrl:'http://localhost:3000/api'
+};
